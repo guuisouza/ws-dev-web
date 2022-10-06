615f2fdc38e5fa5ca57b7c257aa4d1f631e6332e
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-10-06 09:13:21 -0300

    Add files via upload

--- react-hooks/src/exercicios/02.js
+++ react-hooks/src/exercicios/02.js
@@ -3,11 +3,38 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 inicialize o estado como o valor do localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    //Se existir um valor para "name" armazenado no localStorage, use-o como valor inicial para a variável de estado
+    //senão, use o valor da variavel initialName
+    //?? -> operador de coalescência de nulos (se o primeiro valor for nulo usa o segundo)
+
+    //Lazy Initializer (inicializacao preguiçosa)
+    //Faz com que o valor inicial da variavel de estado seja ajustado apenas uma vez,
+    //na fase da montagem.
+    //Para obter esse efeito, basta passar o valor inicial no formato 
+    //de uma função
+    () => obterLocalStorage() ?? initialName
+  )
+
+  function obterLocalStorage(){
+    console.count('Leu localStorage')
+    return window.localStorage.getItem('name')
+  }
 
   // 🐨 Aqui é onde usamos `React.useEffect`.
   // A função deve armazenar `name` no localStorage.
   // 💰 window.localStorage.setItem('name', name)
+
+  //Criando o useEffect
+  //useEffect leva 2 parametros:
+  //01 - Uma função (geralmente no formato arrow function), que será executada como um efeito colateral da atualização do componente
+  //02 -  Um vetor de dependencias (variáveis que serão "vigiadas") por atualizações para disparar o useEffect
+  React.useEffect(() => {
+    //Armazenando o valor da variável de estado name no localStorage
+    window.localStorage.setItem('name', name)
+    //Conta a quantidade de atualizações do componente
+    console.count('atualizei')
+  }, [name]) 
 
   function handleChange(event) {
     setName(event.target.value)
@@ -23,8 +50,8 @@ function Greeting({initialName = ''}) {
   )
 }
 
-function App() {
-  return <Greeting />
+function Exercicio02() {
+  return <Greeting initialName='Turíbio'/>
 }
 
-export default App
+export default Exercicio02
