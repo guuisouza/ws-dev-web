33fee19e66da445abf2c069d181986a94fa3eb56
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-10-20 09:03:27 -0300

    Add files via upload

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -7,7 +7,8 @@ import Exercicio01 from './exercicios/01';
 import Exercicio02 from './exercicios/02';
 import Exercicio03 from './exercicios/03';
 import Exercicio04 from './exercicios/04';
-
+import Exercicio05 from './exercicios/05';
+import Exercicio06 from './exercicios/06';
 function App() {
   return (
     <div className="App">
@@ -23,6 +24,8 @@ function App() {
               <li> <Link to="/02">Exercicio 02</Link> </li>
               <li> <Link to="/03">Exercicio 03</Link> </li>
               <li> <Link to="/04">Exercicio 04</Link> </li>
+              <li> <Link to="/05">Exercicio 05</Link> </li>
+              <li> <Link to="/06">Exercicio 06</Link> </li>
             </ul>
           </nav>
 
@@ -39,6 +42,8 @@ function App() {
             <Route path="/02" element={ <Exercicio02 /> } />
             <Route path="/03" element={ <Exercicio03 /> } />
             <Route path="/04" element={ <Exercicio04 /> } />
+            <Route path="/05" element={ <Exercicio05 /> } />
+            <Route path="/06" element={ <Exercicio06 /> } />
           </Routes>
         </main>
       </BrowserRouter>
