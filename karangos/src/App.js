b7eb3e92fb1588c501342c75def2ee9dd67268a3
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-11-09 11:04:54 -0300

    Add files via upload

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -1,10 +1,39 @@
-
+import { createTheme, ThemeProvider} from '@mui/material'
 import TopBar from './ui/TopBar'
+import Box from '@mui/material/Box'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { yellow, pink } from '@mui/material/colors'
+
+const customTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main:yellow[500]
+    },
+    secondary:{
+      main:pink[500]
+    }
+  }
+})
+
 
 function App() {
   return (
   <>
-    <TopBar/>
+    <ThemeProvider theme={customTheme}>
+      <Box color="secondary" sx={{minHeight: '100vh',
+          backgroundColor:customTheme.palette.background.default}}> 
+
+          <BrowserRouter>
+            <TopBar/>
+
+            <Box component="main" sx={{ m: 24 /*margin: '24px '*/}}>
+              
+            </Box>
+          </BrowserRouter>
+
+        </Box>
+    </ThemeProvider>
   </>
   );
 }
