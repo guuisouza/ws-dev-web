5df5151258dd2603b2a4263cebec38234eac475c
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-11-24 09:16:12 -0300

    Delete karangos directory

--- karangos/src/lib/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-    //A URL PRECISA terminar com /
-    baseURL: 'https://api.faustocintra.com.br/'
-})
-
-export default api
--- karangos/src/pages/KarangoForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function KarangoForm(){
-    return(
-        <>
-            <h1>Cadastro de Karangos</h1>
-        </>
-    )
-}
